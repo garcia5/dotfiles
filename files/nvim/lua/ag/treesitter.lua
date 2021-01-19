38ff754b97b337e1fc7a73b5aa1157b578c86c97
Alexander Garcia <alexander.garcia@factset.com>
2021-01-19 08:30:50 -0500

    WSL changes

--- files/nvim/lua/ag/treesitter.lua
+++ files/nvim/lua/ag/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"python", "typescript", "vue", "lua"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     highlight = {
         enable = true,        -- false will disable the whole extension
     },
