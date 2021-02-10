fba8a3e313212d1e4348a64c2c201269a73fb202
Alexander Garcia <alexander.garcia@factset.com>
2021-02-10 10:17:05 -0500

    grepprg and typescript snippet cleanup

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -6,15 +6,17 @@ vim.g.bulitin_lsp       = true
 require('ag') -- load my lua configs
 
 -- Behaviors
-vim.wo.number     = true                        -- absolute numbers for easier screen sharing
-vim.wo.cursorline = true                        -- highlight current line
-vim.o.belloff     = 'all'                       -- NO BELLS!
-vim.o.completeopt = 'menuone,noinsert,noselect' -- ins-completion how I like it
-vim.o.swapfile    = false                       -- swap files annoy me
-vim.o.inccommand  = 'split'                     -- preview %s commands in a split window as I type
-vim.o.hidden      = true                        -- move away from unsaved buffers
-vim.o.updatetime  = 100                         -- stop typing quickly
-vim.o.undofile    = true                        -- keep track of my 'undo's between sessions
+vim.wo.number     = true                                     -- absolute numbers for easier screen sharing
+vim.wo.cursorline = true                                     -- highlight current line
+vim.o.belloff     = 'all'                                    -- NO BELLS!
+vim.o.completeopt = 'menu,menuone,noselect'                  -- ins-completion how ~I like it~ vsnip likes it
+vim.o.swapfile    = false                                    -- swap files annoy me
+vim.o.inccommand  = 'split'                                  -- preview %s commands in a split window as I type
+vim.o.hidden      = true                                     -- move away from unsaved buffers
+vim.o.updatetime  = 100                                      -- stop typing quickly
+vim.o.undofile    = true                                     -- keep track of my 'undo's between sessions
+vim.o.grepprg     = 'rg --vimgrep --smart-case --no-heading' -- search with rg
+vim.o.grepformat  = '%f:%l:%c:%m'                            -- filename:line number:column number:error message
 
 -- Look and feel
 vim.wo.list                    = true                                               -- show list chars
