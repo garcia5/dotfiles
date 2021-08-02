49ad141f14619d7a89d841f4224773dbfba1bde8
Alexander Garcia <alexander.garcia@factset.com>
2021-08-02 16:16:33 -0400

    style changes
    - No treesitter definition highlight (annoying)
    - Change lsp references keymap
    - Relative filepath in lualine

--- files/nvim/lua/ag/lualine.lua
+++ files/nvim/lua/ag/lualine.lua
@@ -3,7 +3,9 @@ lualine.setup{
     theme = "auto",
     sections = {
         lualine_a = {"mode"},
-        lualine_b = {"filename"},
+        lualine_b = {
+            {"filename", path = 1} -- file w/ relative path
+        },
         lualine_c = {"branch", "diff"},
         lualine_x = {
             {'diagnostics', sources = {'nvim_lsp'}},
@@ -11,4 +13,11 @@ lualine.setup{
         lualine_y = {"filetype"},
         lualine_z = {"location"},
     },
+    options = {
+        disabled_filetypes = {
+            'term'
+        },
+        section_separators = {'', ''},
+        component_separators = {'', ''}
+    }
 }
--- files/nvim/lua/ag/treesitter.lua
+++ files/nvim/lua/ag/treesitter.lua
@@ -60,7 +60,7 @@ require'nvim-treesitter.configs'.setup {
     },
     refactor = {
         highlight_definitions = {
-            enable = true,
+            enable = false,
         },
         smart_rename = {
             enable = true,
