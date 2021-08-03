d8846f8842340fc19f1c075dd5db2d99e4680851
Alexander Garcia <alexander.garcia@factset.com>
2021-08-03 14:51:27 -0400

    statusline configs
    - Disable special status line things in non-normal buffers (terminal,
      help, qf)

--- files/nvim/lua/ag/lualine.lua
+++ files/nvim/lua/ag/lualine.lua
@@ -1,23 +1,50 @@
 local lualine = require('lualine')
+local is_lualine_buf = function ()
+    local cur_buftype = vim.opt.buftype:get()
+    -- Don't load special status line things for these buffer types
+    local disable_for_buftypes = {'terminal', 'help', 'quickfix', 'nofile'}
+
+    for _, buftype in ipairs(disable_for_buftypes) do
+        if buftype == cur_buftype then return false end
+    end
+    return true
+end
+
 lualine.setup{
     theme = "auto",
     sections = {
+        --+-------------------------------------------------+--
+        --| A | B | C                             X | Y | Z |--
+        --+-------------------------------------------------+--
         lualine_a = {"mode"},
-        lualine_b = {
-            {"filename", path = 1} -- file w/ relative path
-        },
-        lualine_c = {"branch", "diff"},
-        lualine_x = {
-            {'diagnostics', sources = {'nvim_lsp'}},
-        },
+        lualine_b = {{
+            "filename",
+            path = 1, -- relative path
+            condition = is_lualine_buf,
+        }},
+        lualine_c = {{
+            "branch", {
+                "diff",
+                symbols = {
+                    modified = '',
+                    removed = '',
+                    added = '',
+            }},
+            condition = is_lualine_buf,
+        }},
+        lualine_x = {{
+            'diagnostics',
+            sources = {'nvim_lsp'},
+            condition = is_lualine_buf,
+        }},
         lualine_y = {"filetype"},
-        lualine_z = {"location"},
+        lualine_z = {{
+            "location",
+            condition = is_lualine_buf,
+        }},
     },
     options = {
-        disabled_filetypes = {
-            'term'
-        },
         section_separators = {'', ''},
-        component_separators = {'', ''}
+        component_separators = {'', ''},
     }
 }
