cd84ca7f9f99f4ec2c07bf80438f0360c9c20135
Alexander Garcia <alexander.garcia@factset.com>
2021-08-10 09:31:39 -0400

    fixes for setup script
    - Test and fix install_packages func
    - Remove outdated .vimrc

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 export CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
-if [[ ! -d "$CONFIG_HOME" ]];
+if [[ ! -d "$CONFIG_HOME" ]]; then
     mkdir -p "$CONFIG_HOME"
 fi
 export NVIM_HOME=$CONFIG_HOME/nvim
 export DF_HOME=$HOME/dotfiles
 
 function usage {
-    echo "USAGE: $0 [bash, brew, nvim, tmux, zsh]"
+    echo "USAGE: $0 [bash, brew, nvim, tmux, zsh, all]"
 }
 
 function backup_file {
@@ -25,8 +25,8 @@ function backup_file {
 
 function backup_dir {
     dir=$1
-    # Symlink, just remove it
     if [[ -d $dir ]]; then
+        # Symlink, just remove it
         if [[ -L $dir ]]; then
             unlink $dir
         else
@@ -46,33 +46,64 @@ function setup_brew {
 
     echo "brew update"
     brew update
-    echo "brew upgrade"
-    brew upgrade
 }
 
 function install_packages {
-    APT_PACKAGES=('liblzma-dev' 'libsqlite3-dev' 'unixodbc-dev') # TODO: add to this
-    BREW_PACKAGES=('gcc' 'fzf' 'bat' 'ripgrep' 'exa' 'pyenv' 'yarn' 'neovim')
+    APT_PACKAGES=( 'liblzma-dev' 'libsqlite3-dev' 'unixodbc-dev' ) # TODO: add to this
+    BREW_PACKAGES=( 'gcc' 'fzf' 'bat' 'ripgrep' 'exa' 'pyenv' 'yarn' 'neovim' 'xz' )
 
-    echo "installing from apt..."
-    echo "sudo apt-get update"
-    sudo apt-get update
-    for pkg in $APT_PACKAGES; do
-        echo "sudo apt-get install $pkg"
-        sudo apt-get install "$pkg"
-    done
+    if [[ $(command -v apt-get) ]]; then
+        echo ""
+        echo "installing from apt..."
+        echo "sudo apt-get update"
+        sudo apt-get update
+        for pkg in ${APT_PACKAGES[@]}; do
+            echo "sudo apt-get install $pkg"
+            sudo apt-get install "$pkg"
+        done
+    fi
 
-    has_brew=$(command -v brew)
-    if [[ $has_brew ]]; then
+    if [[ $(command -v brew) ]]; then
+        echo ""
         echo "installing from brew..."
         brew update
-        for pkg in $BREW_PACKAGES; do
-            echo "brew install $pkg"
-            brew install "$pkg"
+        for pkg in ${BREW_PACKAGES[@]}; do
+            if [[ $(brew list | grep -c "$pkg") -ge 1 ]]; then
+                echo "$pkg already installed, skipping"
+            else
+                echo "brew install $pkg"
+                brew install "$pkg"
+                if [[ "$pkg" == "fzf" ]]; then
+                    # Actually install fzf
+                    $(brew --prefix)/opt/fzf/install
+                fi
+            fi
         done
-        # Actually install fzf
-        $(brew --prefix)/opt/fzf/install
     fi
+
+    echo ""
+    echo "installing from npm..."
+    if [[ -d "$HOME/.nvm" ]]; then
+        echo "nvm is already installed"
+    else
+        echo "installing nvm"
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+        source "$HOME/.bashrc"
+    fi
+    if [[ ! $(command -v node) ]]; then
+        echo "installing node"
+        echo "nvm install node"
+        nvm install node
+    fi
+    NPM_PACKAGES=( 'bash-language-server' 'pyright' 'vls' 'typescript-language-server' )
+    for pkg in ${NPM_PACKAGES[@]}; do
+        if [[ $(npm -g list | grep -c "$pkg") -ge 1 ]]; then
+            echo "$pkg already installed, skipping"
+        else
+            echo "npm i -g $pkg"
+            npm i -g "$pkg"
+        fi
+    done
 }
 
 function setup_bash {
@@ -90,11 +121,15 @@ function setup_bash {
     ln -s "$DF_HOME/files/.bash_aliases" "$file"
 
     # Color schemes
-    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+    if [[ ! $(-d $HOME/.config/base16-shell) ]]; then
+        echo 'Installing colorschemes to $HOME/.config/base16-shell'
+        git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
+    fi
 }
 
 function setup_nvim {
     setup_brew
+    install_packages
     mkdir -p "$HOME/.config"
     backup_dir "$NVIM_HOME"
     ln -s "$DF_HOME/files/nvim" "$NVIM_HOME"
@@ -122,6 +157,7 @@ for conf in "$@"; do
             ;;
         "brew")
             setup_brew
+            install_packages
             ;;
         "nvim")
             setup_nvim
@@ -131,21 +167,22 @@ for conf in "$@"; do
             ;;
         "zsh")
             setup_zsh
-            source "~/.zshrc"
+            source "$HOME/.zshrc"
             ;;
         "all")
             setup_bash
             setup_brew
+            install_packages
             setup_nvim
             setup_tmux
             setup_zsh
-            source "~/.profile"
+            source "$HOME/.profile"
             ;;
         "*")
             usage
             ;;
     esac
 done
-source "~/.profile"
+source "$HOME/.profile"
 echo "Done!"
 exit 0
