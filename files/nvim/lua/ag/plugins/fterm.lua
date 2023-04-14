5c8a8d2b320e33feb3d8fac8d688400e68b1c2c8
Alexander Garcia <agarcia1359@gmail.com>
2023-04-14 08:57:27 -0400

    fterm updates

--- files/nvim/lua/ag/plugins/fterm.lua
+++ files/nvim/lua/ag/plugins/fterm.lua
@@ -1,13 +1,19 @@
 return {
     "numToStr/FTerm.nvim",
     config = function()
+        -- display
+        require("FTerm").setup({
+            border = "rounded",
+            blend = 10, -- a little transparent
+        })
+
         -- basics
         vim.keymap.set("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>')
         vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 
         -- build
         vim.api.nvim_create_user_command("PBuild", function()
-            require("FTerm").scratch({ cmd = { "pnpm", "build" } })
+            require("FTerm").scratch({ cmd = { "pnpm-build" } }) -- my own `pnpm build` alias
             vim.cmd("LspRestart")
         end, { bang = true })
         vim.keymap.set("n", "<Leader>pb", ":PBuild<CR>")
@@ -23,7 +29,7 @@ return {
         -- push
         vim.api.nvim_create_user_command(
             "GPush",
-            function() require("FTerm").scratch({ cmd = { "gpush" } }) end,
+            function() require("FTerm").scratch({ cmd = { "gpush" } }) end, -- my own `git push` alias
             { bang = true }
         )
     end,
