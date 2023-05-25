2b1850a016b967aa87dd7b1f36c3db9f8d1df2aa
Alexander Garcia <agarcia1359@gmail.com>
2023-05-25 09:36:50 -0400

    minor plugin cleanup

--- files/nvim/lua/ag/plugins/sidebar.lua
+++ files/nvim/lua/ag/plugins/sidebar.lua
@@ -1,11 +1,9 @@
 return {
     "sidebar-nvim/sidebar.nvim",
-    config = function()
-        require("sidebar-nvim").setup({
-            ["git"] = {
-                icon = "",
-            },
-            sections = { "datetime", "git" },
-        })
-    end,
+    opts = {
+        ["git"] = {
+            icon = "",
+        },
+        sections = { "datetime", "git" },
+    },
 }
