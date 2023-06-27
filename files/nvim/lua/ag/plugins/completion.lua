739d4c13cc260f41ec056ccca304d96c24973435
Alexander Garcia <agarcia1359@gmail.com>
2023-06-27 15:37:16 -0400

    better cmp ordering (thanks reddit)

--- files/nvim/lua/ag/plugins/completion.lua
+++ files/nvim/lua/ag/plugins/completion.lua
@@ -18,6 +18,32 @@ return {
             snippet = {
                 expand = function(args) require("luasnip").lsp_expand(args.body) end,
             },
+            matching = {
+                disallow_fuzzy_matching = false,
+                disallow_fullfuzzy_matching = false,
+                disallow_partial_fuzzy_matching = false,
+            },
+            sorting = {
+                comparators = {
+                    cmp.config.compare.offset,
+                    cmp.config.compare.exact,
+                    cmp.config.compare.score,
+                    cmp.config.compare.recently_used,
+                    function(entry1, entry2)
+                        local _, entry1_under = entry1.completion_item.label:find("^_+")
+                        local _, entry2_under = entry2.completion_item.label:find("^_+")
+                        entry1_under = entry1_under or 0
+                        entry2_under = entry2_under or 0
+                        if entry1_under > entry2_under then
+                            return false
+                        elseif entry1_under < entry2_under then
+                            return true
+                        end
+                    end,
+                    cmp.config.compare.kind,
+                    cmp.config.compare.sort_text,
+                },
+            },
             sources = cmp.config.sources({
                 { name = "luasnip" },
                 { name = "nvim_lsp_signature_help" },
