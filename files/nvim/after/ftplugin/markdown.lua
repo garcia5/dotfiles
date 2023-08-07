bd9f5ca8dc36518e9d49d249aa00b4d1505b0be6
Alexander Garcia <agarcia1359@gmail.com>
2023-08-07 15:36:42 -0400

    obsidian 'goto file' shortcut

---
+++ files/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,15 @@
+-- Obsidian go to "definition" (file)
+local function get_text_inside_square_brackets()
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local cursor_row = cursor_pos[1]
+
+    local line = vim.fn.getline(cursor_row)
+    local fname = line:match("%[%[(.+)%]%]")
+
+    return fname
+end
+
+vim.keymap.set("n", "<C-]>", function()
+    local fname = get_text_inside_square_brackets() .. ".md"
+    vim.cmd('find ++edit' .. fname)
+end)
