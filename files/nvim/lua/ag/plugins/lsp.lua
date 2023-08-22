6ea946d0a7a84b6844639218c6fd6f59514c10e9
Alexander Garcia <agarcia1359@gmail.com>
2023-08-22 10:49:47 -0400

    separate efm-langserver config from lspconfig

--- files/nvim/lua/ag/plugins/lsp.lua
+++ files/nvim/lua/ag/plugins/lsp.lua
@@ -1,43 +1,6 @@
-return {
+local lsp_config = {
     "neovim/nvim-lspconfig",
     config = function() require("ag.lsp_config") end,
-    dependencies = {
-        {
-            "creativenull/efmls-configs-nvim",
-            version = "v1.x.x",
-            config = function()
-                local custom_attach = require("ag.lsp_config").custom_attach
-                local lspconfig = require("lspconfig")
-
-                local eslint = require("efmls-configs.linters.eslint_d")
-                local prettier = require("efmls-configs.formatters.prettier_d")
-                local stylua = require("efmls-configs.formatters.stylua")
-                local black = require("efmls-configs.formatters.black")
-                local languages = {
-                    lua = { stylua },
-                    typescript = { prettier, eslint },
-                    javascript = { prettier, eslint },
-                    vue = { prettier, eslint },
-                    python = { black },
-                }
-                local efmls_config = {
-                    filetypes = vim.tbl_keys(languages),
-                    settings = {
-                        rootMarkers = { ".git/" },
-                        languages = languages,
-                    },
-                    init_options = {
-                        documentFormatting = true,
-                        documentRangeFormatting = true,
-                    },
-                }
-
-                lspconfig.efm.setup(vim.tbl_extend("force", efmls_config, {
-                    on_attach = function(client, bufnr) custom_attach(client, bufnr, { allowed_clients = { "efm" } }) end,
-                }))
-            end,
-        },
-    },
     ft = {
         "vue",
         "typescript",
@@ -52,4 +15,53 @@ return {
         "zsh",
         "dart",
     },
+}
+
+local efm = {
+    "creativenull/efmls-configs-nvim",
+    version = "v1.x.x",
+    dependencies = { 'neovim/nvim-lspconfig' },
+    config = function()
+        local custom_attach = require("ag.lsp_config").custom_attach
+        local lspconfig = require("lspconfig")
+
+        local eslint = require("efmls-configs.linters.eslint_d")
+        local prettier = require("efmls-configs.formatters.prettier_d")
+        local stylua = require("efmls-configs.formatters.stylua")
+        local black = require("efmls-configs.formatters.black")
+        local languages = {
+            lua = { stylua },
+            typescript = { prettier, eslint },
+            javascript = { prettier, eslint },
+            vue = { prettier, eslint },
+            python = { black },
+        }
+        local efmls_config = {
+            filetypes = vim.tbl_keys(languages),
+            settings = {
+                rootMarkers = { ".git/" },
+                languages = languages,
+            },
+            init_options = {
+                documentFormatting = true,
+                documentRangeFormatting = true,
+            },
+        }
+
+        lspconfig.efm.setup(vim.tbl_extend("force", efmls_config, {
+            on_attach = function(client, bufnr) custom_attach(client, bufnr, { allowed_clients = { "efm" } }) end,
+        }))
+    end,
+    ft = {
+        "lua",
+        "typescript",
+        "javascript",
+        "vue",
+        "python",
+    },
+}
+
+return {
+    lsp_config,
+    efm,
 }
