87d0c61348429177ff345349b6c6e8a8b193799f
Alexander Garcia <agarcia1359@gmail.com>
2023-09-27 10:36:32 -0400

    fix spotify icon

--- files/sketchybar/items/now_playing.sh
+++ files/sketchybar/items/now_playing.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 sketchybar -m --add event spotify_change "com.spotify.client.PlaybackStateChanged" \
-              --add item spotify "${SKETCHYBAR_SPOTIFY_LOC:center}"                \
+              --add item spotify "${SKETCHYBAR_SPOTIFY_LOC:-center}"               \
               --set spotify label.color=$GREEN                                     \
                             script="$PLUGIN_DIR/spotify.sh"                        \
                             click_script="open -a spotify"                         \
