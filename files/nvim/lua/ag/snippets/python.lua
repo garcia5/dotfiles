eb0521bef65e86aa212f5eea84d2e77b7324e874
Alexander Garcia <agarcia1359@gmail.com>
2024-02-27 19:52:10 -0500

    cleanup python snippets

--- files/nvim/lua/ag/snippets/python.lua
+++ files/nvim/lua/ag/snippets/python.lua
@@ -8,21 +8,6 @@ local c = ls.choice_node
 local r = ls.restore_node
 local fmt = require("luasnip.extras.fmt").fmt
 
-local py_function_fmt = [[
-def {name}({params}){ret}:
-	{doc}{body}
-]]
-
-local if_name_main_fmt = [[
-if __name__ == "__main__":
-	{main}
-]]
-
-local class_fmt = [[
-class {cls}{parent}:
-	{doc}{body}
-]]
-
 ---Choice snippet to toggle between python docstring and empty text node
 ---@param order number where in the root snippet the docstring appears
 ---@param default 'DOC' | 'NONE' the default choice to be returned
@@ -43,49 +28,73 @@ local optional_docstring_choice = function(order, default)
     })
 end
 
+---
+local function_fmt = [[
+def {name}({params}){ret}:
+	{doc}{body}
+]]
+local function_snippet = s(
+    "def",
+    fmt(function_fmt, {
+        doc = optional_docstring_choice(1, "DOC"),
+        name = c(2, {
+            r(nil, "func_name", i(nil, "public_function")),
+            sn(nil, {
+                t("_"),
+                r(1, "func_name", i(nil, "private_function")),
+            }),
+        }),
+        params = i(3),
+        ret = c(4, {
+            sn(nil, {
+                t({ " -> " }),
+                r(1, "return_type", i(nil, "None")),
+            }),
+            t(""),
+        }),
+        body = i(0, "..."),
+    }, {
+        stored = {
+            ["return_type"] = i(nil, "None"),
+            ["func_name"] = i(nil, "function_name"),
+        },
+    })
+)
+---
+
+---
+local class_fmt = [[
+class {cls}{parent}:
+	{doc}{body}
+]]
+local class_snippet = s(
+    "class",
+    fmt(class_fmt, {
+        doc = optional_docstring_choice(1, "DOC"),
+        cls = i(2, "ClassName"),
+        parent = c(3, {
+            t(""),
+            sn(nil, {
+                t("("),
+                i(1, "ParentClass"),
+                t(")"),
+            }),
+        }),
+        body = i(0, "..."),
+    })
+)
+---
+
+---
+local if_name_main_fmt = [[
+if __name__ == "__main__":
+	{main}
+]]
+local if_name_main_snippet = s("inm", fmt(if_name_main_fmt, { main = i(0) }))
+---
+
 return {
-    s(
-        "def",
-        fmt(py_function_fmt, {
-            doc = optional_docstring_choice(1, "DOC"),
-            name = c(2, {
-                r(nil, "func_name", i(nil, "public_function")),
-                sn(nil, {
-                    t("_"),
-                    r(1, "func_name", i(nil, "private_function")),
-                }),
-            }),
-            params = i(3),
-            ret = c(4, {
-                sn(nil, {
-                    t({ " -> " }),
-                    r(1, "return_type", i(nil, "None")),
-                }),
-                t(""),
-            }),
-            body = i(0, "..."),
-        }, {
-            stored = {
-                ["return_type"] = i(nil, "None"),
-                ["func_name"] = i(nil, "function_name"),
-            },
-        })
-    ),
-    s(
-        "class",
-        fmt(class_fmt, {
-            doc = optional_docstring_choice(1, "DOC"),
-            cls = i(2, "ClassName"),
-            parent = c(3, {
-                t(""),
-                sn(nil, {
-                    t("("),
-                    i(1, "ParentClass"),
-                    t(")"),
-                }),
-            }),
-            body = i(0, "..."),
-        })
-    ),
-    s("inm", fmt(if_name_main_fmt, { main = i(0) })),
+    function_snippet,
+    class_snippet,
+    if_name_main_snippet,
 }
