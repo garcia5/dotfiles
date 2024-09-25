ce7aa8a901598b8e67e68582d08ae6d4c928a4b2
Alexander Garcia <agarcia1359@gmail.com>
2024-09-25 09:57:24 -0400

    fixup lua ls config

--- files/nvim/lua/ag/lsp_config.lua
+++ files/nvim/lua/ag/lsp_config.lua
@@ -144,37 +144,31 @@ lspconfig.lua_ls.setup({
         M.custom_attach(client, bufnr, { allowed_clients = { "efm" }, format_on_save = true })
     end,
     on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-            client.config.settings = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                    version = "LuaJIT",
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                    library = { vim.env.VIMRUNTIME },
-                    -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                    -- library = vim.api.nvim_get_runtime_file("", true)
-                },
-            })
-
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then return end
         end
-        return true
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                },
+            },
+        })
     end,
     settings = {
         Lua = {
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
                 globals = { "vim" },
-            },
-            workspace = {
-                checkThirdParty = false,
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
             },
         },
     },
