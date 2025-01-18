ad02b108fe9569e4a5cfc029667c9c299bb6fb56
Alexander Garcia <agarcia1359@gmail.com>
2025-01-18 13:14:31 -0500

    lots of blink improvements

--- files/nvim/lua/ag/plugins/blink.lua
+++ files/nvim/lua/ag/plugins/blink.lua
@@ -9,45 +9,84 @@ return {
     -- use a release tag to download pre-built binaries
     version = "v0.*",
     opts = {
-        keymap = { preset = "default" },
+        -- disable for special buffers
+        enabled = function() return not vim.tbl_contains({ "TelescopePrompt", "CopilotChat" }, vim.bo.filetype) end,
+
+        -- For the most part keep default keymaps, but trigger with <C-h> instead of <C-space>
+        keymap = {
+            preset = "default",
+            ["<C-h>"] = { "show", "show_documentation", "hide_documentation" },
+            ["<C-e>"] = { "hide" },
+            ["<C-y>"] = { "select_and_accept" },
+
+            ["<C-p>"] = { "select_prev", "fallback" },
+            ["<C-n>"] = { "select_next", "fallback" },
+
+            ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+            ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+            -- Use <Tab> to trigger and <CR> to accept for command line
+            cmdline = {
+                preset = "enter",
+                ["<Tab>"] = { "show" },
+            },
+        },
+
         appearance = {
             nerd_font_variant = "mono",
         },
 
         sources = {
-            completion = {
-                enabled_providers = { "luasnip", "lsp", "path", "buffer" },
-            },
-            providers = {
-                luasnip = {
-                    name = "Luasnip",
-                    module = "blink.cmp.sources.luasnip",
-                    score_offset = 3,
-                    opts = {
-                        -- Whether to use show_condition for filtering snippets
-                        use_show_condition = true,
-                        -- Whether to show autosnippets in the completion list
-                        show_autosnippets = true,
-                    },
-                },
-            },
-            -- default = { "luasnip", "lsp", "path", "buffer" },
+            default = { "snippets", "lsp", "path", "buffer" },
         },
 
         completion = {
-            menu = {
-                border = "single",
-            },
+            -- disable automatically adding brackets on accept: really annoying for adding imports
+            accept = { auto_brackets = { enabled = false } },
+
             documentation = {
+                -- show documentation popup by default
                 auto_show = true,
+            },
+
+            -- show ghost text of top completion item
+            ghost_text = { enabled = true },
+
+            -- use entire word under cursor for completion, not just the part before the cursor
+            keyword = { range = "full" },
+
+            list = {
+                -- Limit number of results shown in menu
+                max_items = 25,
+                -- allow cycling on either end of the list
+                cycle = {
+                    from_bottom = true,
+                    from_top = true,
+                },
+            },
+
+            menu = {
+                -- Hide completion menu by default, let ghost text take care of the common case for me without the clutter of the menu
+                auto_show = false,
+                -- menu looks more like nvim-cmp
+                border = "single",
+                draw = {
+                    columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },
+                },
             },
         },
 
-        -- experimental signature help support
-        signature = { enabled = true },
+        -- Enable experimental signature help support
+        signature = {
+            enabled = true,
+            window = {
+                border = "single",
+            },
+        },
 
         -- enable luasnip as snippet backend
         snippets = {
+            preset = "luasnip",
             expand = function(snippet) require("luasnip").lsp_expand(snippet) end,
             active = function(filter)
                 if filter and filter.direction then return require("luasnip").jumpable(filter.direction) end
