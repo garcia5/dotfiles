8479cc56aa046a6ff8af78fb69244f05be374af7
Alexander Garcia <agarcia1359@gmail.com>
2025-01-30 22:20:49 -0500

    Update copilot plugin to use `init` instead of `config`

--- files/nvim/lua/ag/plugins/copilot.lua
+++ files/nvim/lua/ag/plugins/copilot.lua
@@ -52,14 +52,12 @@ local copilot = {
             ["*"] = true,
         },
     },
-    config = function(opts)
+    init = function()
         -- auto show/hide when cmp menu is active
         local success, cmp = pcall(require, "cmp")
         if not success then return end
         cmp.event:on("menu_opened", function() vim.b.copilot_suggestion_hidden = true end)
         cmp.event:on("menu_closed", function() vim.b.copilot_suggestion_hidden = false end)
-
-        require("copilot").setup(opts)
     end,
 }
 
