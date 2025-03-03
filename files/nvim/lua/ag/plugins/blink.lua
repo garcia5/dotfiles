dc562173f0cfa33c621116004ab0d1ea711758f8
Alexander Garcia <agarcia1359@gmail.com>
2025-03-03 09:38:22 -0500

    blink updates
    - always fallback to buffer completion
    - disable yamlls completion, it doesn't ever provide anything

--- files/nvim/lua/ag/plugins/blink.lua
+++ files/nvim/lua/ag/plugins/blink.lua
@@ -18,7 +18,7 @@ return {
             preset = "default",
             ["<C-h>"] = { "show", "show_documentation", "hide_documentation" },
             ["<C-e>"] = { "hide", "fallback" },
-            ["<C-y>"] = { "select_and_accept" },
+            ["<C-y>"] = { "select_and_accept", "fallback" },
 
             ["<C-p>"] = { "select_prev", "fallback" },
             ["<C-n>"] = { "select_next", "fallback" },
@@ -32,12 +32,28 @@ return {
         },
 
         sources = {
-            default = { "snippets", "lsp", "path", "buffer", "markdown" },
+            default = { "snippets", "lsp", "path", "buffer" },
+            per_filetype = { yaml = { "path", "buffer" } },
+            min_keyword_length = 0,
             providers = {
                 markdown = {
                     name = "RenderMarkdown",
                     module = "render-markdown.integ.blink",
                     fallbacks = { "lsp" },
+                },
+                lsp = {
+                    name = "LSP",
+                    module = "blink.cmp.sources.lsp",
+                    fallbacks = { "buffer" },
+                },
+                path = {
+                    name = "Path",
+                    module = "blink.cmp.sources.path",
+                    fallbacks = { "buffer" },
+                    opts = {
+                        -- complete path from vim CWD rather than buffer directory
+                        get_cwd = function(_) return vim.fn.getcwd() end,
+                    },
                 },
             },
         },
