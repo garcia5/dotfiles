0e48e65f265fbb0cb1a5cfc914100e1d60dfe95c
Alexander Garcia <agarcia1359@gmail.com>
2025-03-25 15:50:40 -0400

    notify on forecast finished

--- files/nvim/after/ftplugin/yaml.lua
+++ files/nvim/after/ftplugin/yaml.lua
@@ -8,3 +8,8 @@ vim.keymap.set(
     "<cmd>Forecast! %<CR>",
     { silent = true, noremap = true, desc = "Run forecast cli on current file", buffer = 0 }
 )
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "ForecastFinished",
+    callback = function() vim.notify("Forecast Complete", vim.log.levels.INFO) end,
+})
