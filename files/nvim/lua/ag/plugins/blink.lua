009389f5668157c2cdf76d1ba6da6153f351d52b
Alexander Garcia <agarcia1359@gmail.com>
2026-01-22 11:09:28 -0500

    nvim: use built in snippets for lsp snippet expansion

--- files/nvim/lua/ag/plugins/blink.lua
+++ files/nvim/lua/ag/plugins/blink.lua
@@ -3,10 +3,6 @@ return {
     event = "InsertEnter",
     dependencies = {
         {
-            "L3MON4D3/LuaSnip",
-            version = "v2.*",
-        },
-        {
             "fang2hou/blink-copilot", -- copilot integration
             opts = {
                 auto_refresh = false,
@@ -133,13 +129,7 @@ return {
 
         -- enable luasnip as snippet backend
         snippets = {
-            preset = "luasnip",
-            expand = function(snippet) require("luasnip").lsp_expand(snippet) end,
-            active = function(filter)
-                if filter and filter.direction then return require("luasnip").jumpable(filter.direction) end
-                return require("luasnip").in_snippet()
-            end,
-            jump = function(direction) require("luasnip").jump(direction) end,
+            preset = "default",
         },
     },
     -- allows extending the providers array elsewhere in your config
