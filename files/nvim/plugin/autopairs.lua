ba9c2f7863912c50597865edd7cc7ab363b64d65
Alexander Garcia <agarcia1359@gmail.com>
2026-04-23 11:28:07 -0400

    nvim: stop <CR> in insert mode from selecting autocomplete

--- files/nvim/plugin/autopairs.lua
+++ files/nvim/plugin/autopairs.lua
@@ -1,10 +1,18 @@
 vim.pack.add({ "gh:windwp/nvim-autopairs", "gh:windwp/nvim-ts-autotag" })
 
 require("nvim-autopairs").setup({
-    map_cr = true, -- send closing symbol to its own line
+    map_cr = false, -- we handle CR ourselves below
     check_ts = true, -- use treesitter
     map_bs = true,
 })
+
+-- Custom <CR> that dismisses the completion menu instead of accepting a match
+vim.keymap.set("i", "<CR>", function()
+    if vim.fn.pumvisible() ~= 0 then
+        return vim.api.nvim_replace_termcodes("<C-e>", true, false, true) .. require("nvim-autopairs").autopairs_cr()
+    end
+    return require("nvim-autopairs").autopairs_cr()
+end, { expr = true, noremap = true, silent = true, desc = "CR: dismiss completion + autopairs" })
 
 require("nvim-ts-autotag").setup({
     opts = {
